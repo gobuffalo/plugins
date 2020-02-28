8cb52838b7cd67bde9fe720e38bfc1a0f0643d41
Mark Bates <mark@markbates.com>
2020-02-28 12:29:27 -0500

    implement sort on plugins

--- plugins.go
+++ plugins.go
@@ -1,10 +1,34 @@
 package plugins
 
+import "sort"
+
 // Plugins is a slice of type `Plugin` that provides
 // additional useful functionality.
 type Plugins []Plugin
 
+// Len is the number of elements in the collection.
+func (p Plugins) Len() int {
+	return len(p)
+}
+
+// Less reports whether the element with
+// index i should sort before the element with index j.
+func (p Plugins) Less(i int, j int) bool {
+	return p[i].PluginName() < p[j].PluginName()
+}
+
+// Swap swaps the elements with indexes i and j.
+func (p Plugins) Swap(i int, j int) {
+	p[i], p[j] = p[j], p[i]
+}
+
 // ScopedPlugins implements Scoper, return itself.
 func (plugs Plugins) ScopedPlugins() []Plugin {
 	return plugs
+}
+
+// Sort will sort a slice of plugins, by wrapping them
+// in Plugins and using it's sort.Interface implementation.
+func Sort(plugs []Plugin) {
+	sort.Sort(Plugins(plugs))
 }
--- plugins_test.go
+++ plugins_test.go
@@ -1,1 +1,47 @@
 package plugins
+
+import (
+	"sort"
+	"strings"
+	"testing"
+)
+
+func Test_Plugins_Sort(t *testing.T) {
+	plugs := Plugins{
+		back("b"),
+		back("c"),
+		back("a"),
+	}
+
+	sort.Sort(plugs)
+
+	exp := []string{"a", "b", "c"}
+	var act []string
+	for _, p := range plugs {
+		act = append(act, p.PluginName())
+	}
+
+	if strings.Join(act, ",") != strings.Join(exp, ",") {
+		t.Fatalf("Expected %q, got %q", exp, act)
+	}
+}
+
+func Test_Sort(t *testing.T) {
+	plugs := []Plugin{
+		back("b"),
+		back("c"),
+		back("a"),
+	}
+
+	Sort(plugs)
+
+	exp := []string{"a", "b", "c"}
+	var act []string
+	for _, p := range plugs {
+		act = append(act, p.PluginName())
+	}
+
+	if strings.Join(act, ",") != strings.Join(exp, ",") {
+		t.Fatalf("Expected %q, got %q", exp, act)
+	}
+}
